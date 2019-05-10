96fecb91e210b7d20a04d6270e8cacb366d3e9bb
ammcamaz <ammcamaz@gmail.com>
2019-05-10 14:37:08 +0200

    creation du mouv_possible

--- Cavalier.java
+++ Cavalier.java
@@ -11,4 +11,26 @@ class Cavalier extends Piece
   {
     super(Cavalier);
   }
+
+  public boolean mouv_possible(int x , int y)
+  {
+  	int mouv_x=x-this.get_x(); //de combien de case il bouge sur l'axe x
+    int mouv_y=x-this.get_y(); //de combien de case il bouge sur l'axe y
+
+    mouv_x=Math.abs(mouv_x);//valeur absolue de mouv_x
+    mouv_y=Math.abs(mouv_y);//valeur absolue de mouv_y
+
+
+    if (mouv_x==1 && mouv_y==2)
+    {
+      return true;
+    }
+
+    else if (mouv_x==2 && mouv_y==1)
+    {
+      return true;
+    }
+	return false;
+  }
+
 }
