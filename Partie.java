ad91bd4dbbf08a9407b0ddb886e47db3808418a8
ammcamaz <ammcamaz@gmail.com>
2019-05-10 17:41:41 +0200

    historique et sauv fini

--- Partie.java
+++ Partie.java
@@ -5,33 +5,55 @@ public class Partie
   Echequier e;
   int joueur1;//0 pour noir et 1 pour blanc
   int joueur2;//0 pour noir et 1 pour blanc
+  private ArrayList<Histoire> histoire;
 
   public Partie()
   {
     this.e=new Echequier();
     this.joueur1=0;
     this.joueur2=0;
+    ArrayList<Histoire> histoire = new ArrayList<String>();
   }
 
   public void jouer(){}
 
-  public void sauvegard( Piece p , int x , int y)
-  {
-    try
-    {
-      PrintWriter aEcrie = new PrintWriter(BufferedWiriter(new FileWiter("partie_Sauv.txt")));
-      aEcrie.write(this.piece , this.x , this.y);
-      aEcrie.close();
-    }
-    catch (IOException d)
-    {
-      System.out.println("ERREUR" + d);
-    }
-  }
 
+
+    public void Historique(Piece p,int x, int y)
+    {
+      String ch= p.toString()+","+x+","+y;
+      histoire.add(ch);
+
+
+    }
+
+  public void sauvegard()
+  {
+    //try
+    //{
+     FileOutputStream fichier = new FileOutputStream("historique.txt");
+     ObjetOutputStream out = new ObjetOutputStream(fichier);
+     out.writeObjet(al);
+     out.close();
+     fichier.close();
+     System.out.println("La partie est sauvegarder.");
+    /*}
+
+   catch (FileOutFoundException o )
+   {
+     o.printStackTrace();
+   }
+
+   catch (IOException i)
+   {
+     i.printStackTrace();
+   }*/
+
+ }
   public void chargement()
   {
-    String path = "partie_Sauv.txt"
+    String path = "historique.txt"
+
 
   }
 }
