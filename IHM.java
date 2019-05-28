6fd7ca781671ae1845b1ed0df369cf334e417e24
David-Charlesia <pro.charlesia.david@outlook.com>
2019-05-28 11:03:10 +0200

    aff bouton provi ok(position correct)

--- IHM.java
+++ IHM.java
@@ -15,15 +15,17 @@ public class IHM extends JFrame
     this.setLayout(new BorderLayout());
     this.add(this.getPanelCentre(e),BorderLayout.CENTER);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setBounds(300,300,600,400);
     this.setVisible(true);
   }
 
   public JPanel getPanelCentre(Echequier e)
   {
     JPanel jp=new JPanel();
+    jp.setLayout(new GridLayout(8,8));
     JButton bt;
     BoutonListener blis=new BoutonListener();
-
+    int couleur=1;
     for(int i=0;i<64;i++)
     {
       if(e.get_case(i)==null)
@@ -33,6 +35,35 @@ public class IHM extends JFrame
       {
         bt=new JButton(e.get_case(i).toString());
       }
+
+      if(couleur==1)
+      {
+        if(i%8==0)
+        {
+          bt.setBackground(Color.WHITE);
+          bt.setForeground(Color.BLACK);
+          couleur=1;
+        }else
+        {
+          bt.setBackground(Color.BLACK);
+          bt.setForeground(Color.WHITE);
+          couleur=0;
+        }
+      }else
+      {
+        if(i%8==0)
+        {
+          bt.setBackground(Color.BLACK);
+          bt.setForeground(Color.WHITE);
+          couleur=0;
+        }else
+        {
+          bt.setBackground(Color.WHITE);
+          bt.setForeground(Color.BLACK);
+          couleur=1;
+        }
+      }
+
       bt.addActionListener(blis);
 
       jp.add(bt);
